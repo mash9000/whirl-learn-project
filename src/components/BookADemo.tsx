4ca10abe1d0838386cbb1984d0359f1a4f4bfc0d
simmetr <vm.certo@gmail.com>
2025-12-29 22:29:16 +0700

    style: book a demo стилизован

--- src/components/BookADemo.tsx
+++ src/components/BookADemo.tsx
@@ -1,4 +1,6 @@
 import '../styles/book-a-demo/book-a-demo.scss';
+import '../styles/book-a-demo/book-a-demo--hover.scss';
+import '../styles/book-a-demo/book-a-demo--active.scss';
 import type {IBookADemoButton} from "../model/interfaces/IBookADemoButton.ts";
 
 type BookADemoProps = {
@@ -7,6 +9,6 @@ type BookADemoProps = {
 
 export const BookADemo = ({bookADemo}: BookADemoProps) => {
     return (
-        <button className='book-a-demo'>{bookADemo.getString()}</button>
+        <button className='book-a-demo book-a-demo--hover book-a-demo--active'>{bookADemo.getString()}</button>
     )
 }
