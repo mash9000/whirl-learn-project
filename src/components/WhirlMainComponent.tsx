b1b45f25d55c5a8e939139fdda44d4705d15990d
simmetr <vm.certo@gmail.com>
2025-12-29 15:47:39 +0700

    refactor

--- src/components/WhirlMainComponent.tsx
+++ src/components/WhirlMainComponent.tsx
@@ -1,15 +1,9 @@
 import '../styles/whirl-main-component/whirl-main-component.scss';
 import {Header} from "./Header.tsx";
-import {NavbarController} from "../model/implementation/NavbarController.ts";
+import {getNavbarGetPoints} from "../data/NavbarGetPoints.ts";
 
 export const WhirlMainComponent = () => {
-    const navbarController = new NavbarController();
-    navbarController.addPoint('Features', new URL('http://localhost:8080'));
-    navbarController.addPoint('Pricing', new URL('http://localhost:8080'));
-    navbarController.addPoint('Integrations', new URL('http://localhost:8080'));
-    navbarController.addPoint('Learn', new URL('http://localhost:8080'));
-
     return (
-        <Header points={navbarController.getPoints()}/>
+        <Header points={getNavbarGetPoints().getPoints()}/>
     )
 }
