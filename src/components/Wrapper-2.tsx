74e17820de610338d6d319d91fe66d6714cbfecf
simmetr <vm.certo@gmail.com>
2026-01-01 23:00:25 +0700

    style: временная фиксация для создания единого центра оформления текста

--- src/components/Wrapper-2.tsx
+++ src/components/Wrapper-2.tsx
@@ -1,4 +1,8 @@
 import '../styles/wrapper-2/wrapper-2.scss';
+import '../styles/wrapper-2/__box/wrapper-2__box.scss';
+import '../styles/wrapper-2/__heading/wrapper-2__heading.scss';
+import '../styles/wrapper-2/__p/wrapper-2__p.scss';
+import '../styles/wrapper-2/__subbox/wrapper-2__subbox.scss';
 import type {IWrapper2} from "../model/interfaces/IWrapper2.ts";
 import {BookADemo} from "./BookADemo.tsx";
 import type {IBookADemoButton} from "../model/interfaces/IBookADemoButton.ts";
@@ -11,13 +15,19 @@ type Wrapper2Props = {
 export const Wrapper2 = ({data, bookADemo}: Wrapper2Props) => {
     return (
         <div className='wrapper-2'>
-            <h3 className='wrapper-2__heading'>{data.getHeading()}</h3>
-            <p className='wrapper-2__p'>{data.getParagraph()}</p>
-            <ol>
-                {data.getList().map((item: string) => <li
-                    key={item}>{item}</li>)}
-            </ol>
-            <BookADemo bookADemo={bookADemo}/>
+            <div className='wrapper-2__subbox'>
+                <img alt={`${data.getBgName()} image`}
+                     src='../../public/images/wrappers/light-bulb.svg'/>
+                <div className='wrapper-2__box'>
+                    <h3 className='wrapper-2__heading'>{data.getHeading()}</h3>
+                    <p className='wrapper-2__p'>{data.getParagraph()}</p>
+                    <ol>
+                        {data.getList().map((item: string) => <li
+                            key={item}>{item}</li>)}
+                    </ol>
+                    <BookADemo bookADemo={bookADemo}/>
+                </div>
+            </div>
         </div>
     );
 }
