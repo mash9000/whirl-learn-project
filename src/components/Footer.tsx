ae58545a83ca97f23bf5655c720ffda8dc7d88d4
simmetr <vm.certo@gmail.com>
2026-01-02 18:29:00 +0700

    feat: разметка infobox реализована

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,21 @@ import '../styles/footer/footer.scss';
 import '../styles/footer/__inner-box/footer__inner-box.scss';
 import '../styles/footer/__newsletter/footer__newsletter.scss';
 import '../styles/footer/__admin/footer__admin.scss';
+import '../styles/footer/__infobox/footer__infobox.scss';
 
 export const Footer = () => {
     return (
         <footer className='footer'>
             <div className='footer__inner-box'>
+                <div className='footer__infobox'>
+                    <h3 className='footer__infobox__heading'>Info</h3>
+                    <a href='#' target='_blank' className='footer__infobox__link'>Features</a>
+                    <a href='#' target='_blank' className='footer__infobox__link'>Pricing</a>
+                    <a href='#' target='_blank' className='footer__infobox__link'>Blog</a>
+                    <a href='#' target='_blank' className='footer__infobox__link'>Support</a>
+                    <a href='#' target='_blank' className='footer__infobox__link'>Terms & Conditions</a>
+                    <a href='#' target='_blank' className='footer__infobox__link'>Privacy Policy</a>
+                </div>
                 <div className='footer__admin'>
                     <h3 className='footer__admin__heading'>Admin</h3>
                     <a href='#' target='_blank' className='footer__admin__link'>Style Guide</a>
