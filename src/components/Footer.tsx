bb797a0fe161f7fa7989b0b0b8c861cdc1041b6f
simmetr <vm.certo@gmail.com>
2026-01-02 18:35:31 +0700

    feat: разметка address реализована

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,7 @@ import '../styles/footer/__inner-box/footer__inner-box.scss';
 import '../styles/footer/__newsletter/footer__newsletter.scss';
 import '../styles/footer/__admin/footer__admin.scss';
 import '../styles/footer/__infobox/footer__infobox.scss';
+import '../styles/footer/__address/footer__address.scss';
 
 export const Footer = () => {
     return (
@@ -10,21 +11,33 @@ export const Footer = () => {
             <div className='footer__inner-box'>
                 <div className='footer__infobox'>
                     <h3 className='footer__infobox__heading'>Info</h3>
-                    <a href='#' target='_blank' className='footer__infobox__link'>Features</a>
-                    <a href='#' target='_blank' className='footer__infobox__link'>Pricing</a>
-                    <a href='#' target='_blank' className='footer__infobox__link'>Blog</a>
-                    <a href='#' target='_blank' className='footer__infobox__link'>Support</a>
-                    <a href='#' target='_blank' className='footer__infobox__link'>Terms & Conditions</a>
-                    <a href='#' target='_blank' className='footer__infobox__link'>Privacy Policy</a>
+                    <a href='#' target='_blank'
+                       className='footer__infobox__link'>Features</a>
+                    <a href='#' target='_blank'
+                       className='footer__infobox__link'>Pricing</a>
+                    <a href='#' target='_blank'
+                       className='footer__infobox__link'>Blog</a>
+                    <a href='#' target='_blank'
+                       className='footer__infobox__link'>Support</a>
+                    <a href='#' target='_blank'
+                       className='footer__infobox__link'>Terms & Conditions</a>
+                    <a href='#' target='_blank'
+                       className='footer__infobox__link'>Privacy Policy</a>
                 </div>
                 <div className='footer__admin'>
                     <h3 className='footer__admin__heading'>Admin</h3>
-                    <a href='#' target='_blank' className='footer__admin__link'>Style Guide</a>
-                    <a href='#' target='_blank' className='footer__admin__link'>Licenses</a>
-                    <a href='#' target='_blank' className='footer__admin__link'>Instructions</a>
-                    <a href='#' target='_blank' className='footer__admin__link'>Changelog</a>
-                    <a href='#' target='_blank' className='footer__admin__link'>Password</a>
-                    <a href='#' target='_blank' className='footer__admin__link'>404</a>
+                    <a href='#' target='_blank' className='footer__admin__link'>Style
+                        Guide</a>
+                    <a href='#' target='_blank'
+                       className='footer__admin__link'>Licenses</a>
+                    <a href='#' target='_blank'
+                       className='footer__admin__link'>Instructions</a>
+                    <a href='#' target='_blank'
+                       className='footer__admin__link'>Changelog</a>
+                    <a href='#' target='_blank'
+                       className='footer__admin__link'>Password</a>
+                    <a href='#' target='_blank'
+                       className='footer__admin__link'>404</a>
                 </div>
                 <div className='footer__newsletter'>
                     <h3 className='footer__newsletter__heading'>Newsletter</h3>
@@ -35,6 +48,14 @@ export const Footer = () => {
                         placeholder='Your email address'
                         className='footer__newsletter__input'/>
                 </div>
+                <address className='footer__address'>
+                    <p className='footer__address__rights'>© 2022 Whirl. All Rights Reserved. Illustrations by <a href='#' target='_blank'>Streamline</a></p>
+                    <div className='footer__address__social-networks'>
+                        <a href='#' target='_blank' style={{}}></a>
+                        <a href='#' target='_blank' style={{}}></a>
+                        <a href='#' target='_blank' style={{}}></a>
+                    </div>
+                </address>
             </div>
         </footer>
     );
